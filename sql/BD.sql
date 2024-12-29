1d5e4ed37372fdb73ff118d6eeebb1bb2787e06b
LUIGUI55 <luisantonio6942@gmail.com>
2024-12-28 23:37:06 -0600

    SQL

--- sql/BD.sql
+++ sql/BD.sql
@@ -8,3 +8,16 @@ CREATE TABLE usuarios (
     correo VARCHAR(100) NOT NULL UNIQUE,
     edad INT
 );
+
+INSERT INTO usuarios (nombre, correo, edad)
+VALUES ('Juan Pérez', 'juan@example.com', 30),
+       ('Ana Gómez', 'ana@example.com', 25);
+
+
+SELECT * FROM usuarios;
+
+SELECT * FROM usuarios WHERE edad > 25;
+
+UPDATE usuarios
+SET edad = 31
+WHERE nombre = 'Juan Pérez';
