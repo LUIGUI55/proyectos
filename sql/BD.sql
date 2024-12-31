3a777c2ef2ed5beaa09e087b486a2e13491df9f4
LUIGUI55 <luisantonio6942@gmail.com>
2024-12-30 23:38:15 -0600

    SQL

--- sql/BD.sql
+++ sql/BD.sql
@@ -21,3 +21,25 @@ SELECT * FROM usuarios WHERE edad > 25;
 UPDATE usuarios
 SET edad = 31
 WHERE nombre = 'Juan Pérez';
+
+
+DELETE FROM usuarios
+WHERE edad < 30;
+
+ALTER TABLE usuarios
+ADD COLUMN telefono VARCHAR(15);
+
+ALTER TABLE usuarios
+DROP COLUMN telefono;
+
+CREATE TABLE pedidos (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    usuario_id INT,
+    fecha DATE,
+    monto DECIMAL(10, 2),
+    FOREIGN KEY (usuario_id) REFERENCES usuarios(id)
+);
+
+INSERT INTO pedidos (usuario_id, fecha, monto)
+VALUES (1, '2025-01-13', 150.75),
+       (2, '2025-01-12', 200.00);
