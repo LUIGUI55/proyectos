0bf5a088671869e8a11b76e1c51195450182aefc
LUIGUI55 <luisantonio6942@gmail.com>
2025-01-19 15:35:39 -0600

    python

--- python/csv.py
+++ python/csv.py
@@ -2,4 +2,3 @@ import pandas as pd
 
 df = pd.read_csv("datos.csv")
 print(df.head())  # Muestra las primeras 5 filas
-
--- python/edad.py
+++ python/edad.py
@@ -3,4 +3,3 @@ import pandas as pd
 # Filtrar filas donde la edad sea mayor a 30
 filtro = df[df["Edad"] > 30]
 print(filtro)
-
