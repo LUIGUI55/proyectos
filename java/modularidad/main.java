77af943d810a08aaf154e30f42da8c56d12ffb4a
LUIGUI55 <luisantonio6942@gmail.com>
2025-01-20 16:32:16 -0600

    javaMod

--- java/modularidad/main.java
+++ java/modularidad/main.java
@@ -5,6 +5,12 @@ public class main{
         int resultadoSuma = calc.sumar(5,3);
         int resultadoResta = calc.restar(10,4);
         int resultadoMultiplicacion = calc.multiplicar (2, 6);
-        double resultadoDivicion
+        double resultadoDivicion = calc.dividir(8,2);
+
+        System.out.println("SUMA" + resultadoSuma);
+        System.out.println("RESTA" + resultadoResta);
+        System.out.println("MULTIPLICACION" + resultadoMultiplicacion);
+        System.out.println("DIVICION" + resultadoDivicion);
+
     }
 }
