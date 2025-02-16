95028e05fd6af8d17b2c3f634226688c17fb189e
LUIGUI55 <luisantonio6942@gmail.com>
2025-02-15 20:41:20 -0600

    sh

---
+++ scripts/correo.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Enviar un correo electrónico
+
+TO="destinatario@example.com"
+SUBJECT="Asunto del correo"
+BODY="Cuerpo del correo"
+
+echo "$BODY" | mail -s "$SUBJECT" "$TO"
+echo "Correo enviado a $TO"
