ede9feae1f358d6f69bf9a618baddadc7a04dc2f
LUIGUI55 <luisantonio6942@gmail.com>
2025-02-15 20:43:30 -0600

    sh

---
+++ scripts/scaneopuertos.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Realizar un escaneo de puertos
+
+HOST="127.0.0.1"
+PORTS=(22 80 443)
+
+for PORT in "${PORTS[@]}"; do
+    (echo > /dev/tcp/$HOST/$PORT) &> /dev/null && echo "Puerto $PORT está abierto en $HOST" || echo "Puerto $PORT está cerrado en $HOST"
+done
