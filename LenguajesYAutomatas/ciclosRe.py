11fba17600ce836ba6c1a9c41b01d519aec34b30
LUIGUI55 <luisantonio6942@gmail.com>
2025-03-13 05:23:52 -0600

    API

---
+++ LenguajesYAutomatas/ciclosRe.py
@@ -0,0 +1,16 @@
+import time
+
+def medir_tiempo_ejecucion():
+    inicio = time.process_time_ns()
+
+    for i in range(1000000):
+        pass
+
+    fin = time.process_time_ns()
+    tiempo_ejecucion = fin - inicio
+
+    print(f"Tiempo de ejecución (ciclos de reloj): {tiempo_ejecucion} nanosegundos")
+
+if __name__ == "__main__":
+    medir_tiempo_ejecucion()
+    
